6d759dd4399ef7967d6370224fd5e1fdf7ade277
MateDawid <mateusiakdawid@gmail.com>
2026-06-19 11:43:25 +0200

    move alembic to backend/src

--- backend/src/alembic/env.py
+++ backend/src/alembic/env.py
@@ -7,10 +7,10 @@ from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
-from src.config import settings
-from src.database import Base
+from config import settings
+from database import Base
 # TODO: Test if importing models needed
-# import models  # noqa: F401
+from auth.models import DbUser  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
