704141ebca730c617041328b3fbc33b8dfd76346
MateDawid <mateusiakdawid@gmail.com>
2026-06-22 14:47:16 +0200

    Updata DbUser and its migration

--- backend/src/auth/models/db_user.py
+++ backend/src/auth/models/db_user.py
@@ -1,13 +1,25 @@
-from datetime import UTC, datetime
+import uuid
+from datetime import datetime, UTC
 
-from sqlalchemy import String, UUID
+from sqlalchemy import String
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database import Base
 
+## todo - regenerate migrations
+
 class DbUser(Base):
+    """
+    Represents a user in the database.
+
+    Attributes:
+        id (uuid.UUID): The unique identifier for the user.
+        email (str): The email address of the user.
+        password_hash (str): The hashed password of the user.
+    """
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(UUID, primary_key=True, index=True)
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(200), nullable=False)
