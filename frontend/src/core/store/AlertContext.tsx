6f9ab7137fe7abc7f70b3e4ec47f4c887d3bf0c7
MateDawid <mateusiakdawid@gmail.com>
2026-07-11 07:54:14 +0200

    AlertContext - docstrings update

--- frontend/src/core/store/AlertContext.tsx
+++ frontend/src/core/store/AlertContext.tsx
@@ -1,15 +1,24 @@
 import { createContext, useContext, useState, type JSX, type ReactNode } from 'react';
 
+/**
+ * Alert type definition. It represents the structure of an alert message, including the message text and its type (success, error, info, or warning).
+ */
 type Alert = {
   message: string;
   type: 'success' | 'error' | 'info' | 'warning';
 }
 
+/**
+ * AlertContextType defines the shape of the context value provided by AlertContext. It includes the current alert state and a function to update it.
+ */
 type AlertContextType = {
   alert: Alert | null;
   setAlert: (alert: Alert | null) => void;
 }
 
+/**
+ * ChildrenType defines the props for the AlertProvider component, specifically the children that will be wrapped by the provider.
+ */
 type ChildrenType = {
   children: ReactNode;
 };
@@ -19,16 +28,13 @@ const AlertContext = createContext<AlertContextType | undefined>(undefined);
 /**
  * Provider component for AlertContext. It provides the alert state and setAlert function to its children.
  * 
- * @param children - The child components that will have access to the alert context.
- * @returns The AlertContext.Provider component wrapping the children.
+ * @param {ChildrenType} children The child components that will have access to the alert context.
+ * @returns {JSX.Element} The AlertContext.Provider component wrapping the children.
  */
 export const AlertProvider = ({ children }: ChildrenType): JSX.Element => {
   const [alert, setAlert] = useState<Alert | null>(null);
 
-  const value = {
-    alert,
-    setAlert,
-  };
+  const value = { alert, setAlert };
 
   return (
     <AlertContext.Provider value={value}>{children}</AlertContext.Provider>
@@ -37,11 +43,12 @@ export const AlertProvider = ({ children }: ChildrenType): JSX.Element => {
 
 
 /**
+ * Hook to access the AlertContext. It throws an error if used outside of the AlertProvider.
  * 
- * @returns 
+ * @returns AlertContextType - The current alert state and the setAlert function.
  */
 export const useAlertContext = (): AlertContextType => {
   const context = useContext(AlertContext);
-  if (!context) throw new Error('useAlert must be used within AlertProvider');
+  if (!context) throw new Error('useAlertContext must be used within AlertProvider');
   return context;
 };
