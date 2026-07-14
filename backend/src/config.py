f8ee95ce43d04c669e02d807f4b246961616a998
MateDawid <mateusiakdawid@gmail.com>
2026-07-14 11:16:52 +0200

    ACCESS_TOKEN_EXPIRE_SECONDS

--- backend/src/config.py
+++ backend/src/config.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
 
     secret_key: SecretStr
     algorithm: str = "HS256"
-    access_token_expire_minutes: int = 30
+    access_token_expire_seconds: int
 
 
 settings = Settings()  # type: ignore[call-arg] # Loaded from .env file
