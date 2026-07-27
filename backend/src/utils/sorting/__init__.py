10d4b765ee33b2417dd65967ab2244e43a76605c
MateDawid <mateusiakdawid@gmail.com>
2026-07-27 08:02:09 +0200

    order_by_query_service

--- backend/src/utils/sorting/__init__.py
+++ backend/src/utils/sorting/__init__.py
@@ -1,4 +1,4 @@
 from utils.sorting.services.sorting_service import get_db_query_with_ordering, OrderByField
-from utils.sorting.order_by_query import OrderByQuery
+from utils.sorting.services.order_by_query_service import OrderByQuery
 
 __all__ = ["get_db_query_with_ordering", "OrderByField", "OrderByQuery"]
