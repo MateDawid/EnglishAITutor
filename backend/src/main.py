b45b28d0c4339d985c1c61cda45844ec4cee418c
MateDawid <mateusiakdawid@gmail.com>
2026-08-02 08:10:33 +0200

    Logging config

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,18 +1,21 @@
+import logging.config
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from logging_config import LOGGING_CONFIG
 from utils.database import engine
 from core.router import router as core_router
 from auth.router import router as auth_router
 from flashcards.router import router as flashcards_router
 
+logging.config.dictConfig(LOGGING_CONFIG)
+
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
     yield
-    # Shutdown
     await engine.dispose()
 
 
