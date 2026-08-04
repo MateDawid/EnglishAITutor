6970ad3c9f5966952829c00180f3ce5680a32773
MateDawid <mateusiakdawid@gmail.com>
2026-08-04 12:36:16 +0200

    part_of_speech as filterable field in flashcards endpoint

--- backend/src/flashcards/router.py
+++ backend/src/flashcards/router.py
@@ -2,9 +2,10 @@ from typing import Annotated, Optional
 
 from auth.models import DbUser
 from auth.services.current_user_service import get_current_user_from_db
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from flashcards.models.db_flashcard import PartOfSpeech
 from utils.database import get_db
 from flashcards.schemas import FlashcardSchema
 from flashcards.services.flashcard_service import get_flashcards_from_db
@@ -23,6 +24,7 @@ async def flashcard_list_view(
     order_by: Annotated[Optional[str], OrderByQuery(("word",))] = None,
     word: Annotated[Optional[str], FilterByStringQuery()] = None,
     meaning: Annotated[Optional[str], FilterByStringQuery()] = None,
+    part_of_speech: Annotated[Optional[PartOfSpeech], Query()] = None,
 ) -> PaginatedResponse[FlashcardSchema]:
     """
     View to retrieve the list of flashcards.
@@ -34,10 +36,13 @@ async def flashcard_list_view(
         order_by (str | None): The order by query parameters.
         word (str | None): The "word" field filter.
         meaning (str | None): The "meaning" field filter.
-
+        part_of_speech (str | None): The "part_of_speech" field filter.
     Returns:
         PaginatedResponse[FlashcardSchema]: The paginated result.
     """
     return await get_flashcards_from_db(
-        db=db, pagination_query=pagination_query, order_by=order_by, filters={"word": word, "meaning": meaning}
+        db=db,
+        pagination_query=pagination_query,
+        order_by=order_by,
+        filters={"word": word, "meaning": meaning, "part_of_speech": part_of_speech},
     )
