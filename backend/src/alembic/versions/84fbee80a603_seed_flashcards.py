3eb52ca258761373916e24d593aa07e9dd9ddaf8
MateDawid <mateusiakdawid@gmail.com>
2026-08-05 11:13:51 +0200

    Seed flashcards in db migration conditionally

--- backend/src/alembic/versions/84fbee80a603_seed_flashcards.py
+++ backend/src/alembic/versions/84fbee80a603_seed_flashcards.py
@@ -6,6 +6,7 @@ Create Date: 2026-07-19 06:27:09.842328
 
 """
 
+import logging
 import os
 from typing import Sequence, Union
 
@@ -22,12 +23,16 @@ down_revision: Union[str, Sequence[str], None] = "3ad30277dea3"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
+LOGGER = logging.getLogger(__name__)
+
 
 def upgrade() -> None:
     """Seed flashcards from flashcards.json."""
-    print("MIGRATING")
-    if not os.environ.get("POPULATE_DB"):
+    LOGGER.warning(f"🪲 DEBUG: {os.environ.get('SEED_FLASHCARDS')}")
+    if not os.environ.get("SEED_FLASHCARDS", "").lower() == "true":
+        LOGGER.warning("⚠️ Populating database Flashcards skipped.")
         return
+    LOGGER.warning("ℹ️ Populating database Flashcards from flashcards.json.")
     json_path = Path(__file__).parent.parent / "flashcards.json"
 
     with open(json_path, "r", encoding="utf-8") as f:
