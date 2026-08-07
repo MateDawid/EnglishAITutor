c8b3485e7a9f6d84f6f82a482f2dc4803cec0208
MateDawid <mateusiakdawid@gmail.com>
2026-08-07 11:06:39 +0200

    Separate utils file for Flashcard datagrid

--- frontend/src/flashcards/components/FlashcardsDataGrid.tsx
+++ frontend/src/flashcards/components/FlashcardsDataGrid.tsx
@@ -1,141 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import { useTheme, useMediaQuery } from '@mui/material';
-import {
-  getGridDateOperators,
-  getGridStringOperators,
-  getGridNumericOperators,
-  getGridBooleanOperators,
-  getGridSingleSelectOperators,
-} from '@mui/x-data-grid';
+import { getSortFieldMapping, mappedFilterOperators, PAGE_SIZE_OPTIONS, formatFilterModel } from './FlashcardsDataGrid.utils';
 import { StyledDataGrid } from './FlashcardsDataGrid.styles';
 import apiClient from '../../core/apiClient';
 import { useAlertContext } from '../../core/store/AlertContext';
-
-const pageSizeOptions = [10, 50, 100];
-
-
-/**
- * Mapping of DataGrid columns types with supported filter methods.
- */
-export const mappedFilterOperators = {
-  string: getGridStringOperators().filter((operator) =>
-    ['contains'].includes(operator.value)
-  ),
-  date: getGridDateOperators().filter((operator) =>
-    ['is', 'before', 'onOrBefore', 'after', 'onOrAfter'].includes(
-      operator.value
-    )
-  ),
-  dateTime: getGridDateOperators(true).filter((operator) =>
-    ['is', 'before', 'onOrBefore', 'after', 'onOrAfter'].includes(
-      operator.value
-    )
-  ),
-  number: getGridNumericOperators().filter((operator) =>
-    ['=', '>=', '<='].includes(operator.value)
-  ),
-  boolean: getGridBooleanOperators(),
-  singleSelect: getGridSingleSelectOperators().filter((operator) =>
-    ['is'].includes(operator.value)
-  ),
-};
-
-/**
- * Function for formatting filterModel for API calls purposes.
- * @param {object} updatedFilterModel - Updated filterModel returned from DataGrid after update.
- * @param {object} columns - Displayed columns settings.
- * @return {object} - Formatted filterModel for DataGrid.
- */
-export function formatFilterModel(updatedFilterModel, columns) {
-  if (updatedFilterModel.items.length === 0) {
-    return {};
-  } else if (updatedFilterModel.items[0].value == null) {
-    return {};
-  }
-  const filterItem = updatedFilterModel.items[0];
-  const column = columns.find((column) => column.field === filterItem.field);
-  switch (column.type) {
-    case 'date':
-      return formatDateFilter(filterItem);
-    case 'number':
-      return formatNumberFilter(filterItem);
-    default:
-      return { [filterItem.field]: filterItem.value };
-  }
-}
-
-/**
- * Function for formatting date filter for API calls purposes.
- * @param {object} filterItem - Filter definition received from DataGrid
- * @return {object} - Formatted filterModel with date filter for DataGrid.
- */
-function formatDateFilter(filterItem) {
-  switch (filterItem.operator) {
-    case 'is':
-      return {
-        [`${filterItem.field}_after`]: formatDate(filterItem.value),
-        [`${filterItem.field}_before`]: formatDate(filterItem.value),
-      };
-    case 'before': {
-      let dayBefore = new Date(filterItem.value);
-      dayBefore.setDate(dayBefore.getDate() - 1);
-      return { [`${filterItem.field}_before`]: formatDate(dayBefore) };
-    }
-    case 'onOrBefore':
-      return { [`${filterItem.field}_before`]: formatDate(filterItem.value) };
-    case 'after': {
-      let dayAfter = new Date(filterItem.value);
-      dayAfter.setDate(dayAfter.getDate() + 1);
-      return { [`${filterItem.field}_after`]: formatDate(dayAfter) };
-    }
-    case 'onOrAfter':
-      return { [`${filterItem.field}_after`]: formatDate(filterItem.value) };
-    default:
-      return { [filterItem.field]: filterItem.value };
-  }
-}
-
-/**
- * Function for formatting Date object into string in format YYYY-MM-DD
- * @param {Date} date - Filter definition received from DataGrid
- * @return {string} - Formatted filterModel with date filter for DataGrid.
- */
-function formatDate(date) {
-  // TODO - use it in all places where formatting appears
-  return date.toLocaleDateString('en-CA');
-}
-
-/**
- * Function for formatting number filter for API calls purposes.
- * @param {object} filterItem - Filter definition received from DataGrid
- * @return {object} - Formatted filterModel with number filter for DataGrid.
- */
-function formatNumberFilter(filterItem) {
-  switch (filterItem.operator) {
-    case '>=': {
-      return { [`${filterItem.field}_min`]: filterItem.value };
-    }
-    case '<=':
-      return { [`${filterItem.field}_max`]: filterItem.value };
-    default:
-      return { [filterItem.field]: filterItem.value };
-  }
-}
-
-/**
- * Function to prepare mapping of API ordering fields for DataTable columns other than column names.
- * @param {object} columns - DataTable columns definitions.
- * @return {object} - Mapping for sorting DataTable rows.
- */
-const getSortFieldMapping = (columns) => {
-  return columns.reduce((acc, column) => {
-    if (column.sortField) {
-      acc[column.field] = column.sortField;
-    }
-    return acc;
-  }, {});
-};
 
 
 
@@ -157,7 +26,7 @@ const FlashcardsDataGrid = () => {
   // Loading and pagination
   const [loading, setLoading] = useState(true);
   const [paginationModel, setPaginationModel] = React.useState({
-    pageSize: pageSizeOptions[0],
+    pageSize: PAGE_SIZE_OPTIONS[0],
     page: 0,
   });
 
@@ -184,17 +53,7 @@ const FlashcardsDataGrid = () => {
       align: 'left',
       flex: 2,
       filterable: true,
-      sortable: false,
-    },
-    {
-      field: 'example',
-      type: 'string',
-      headerName: 'Example',
-      headerAlign: 'center',
-      align: 'left',
-      flex: 2,
-      filterable: true,
-      sortable: false,
+      sortable: true,
     },
     {
       field: 'part_of_speech',
@@ -308,7 +167,7 @@ const FlashcardsDataGrid = () => {
         paginationMode="server"
         paginationModel={paginationModel}
         onPaginationModelChange={updatePagination}
-        pageSizeOptions={pageSizeOptions}
+        pageSizeOptions={PAGE_SIZE_OPTIONS}
         sortingMode="server"
         onSortModelChange={updateSorting}
         filterMode="server"
---
+++ frontend/src/flashcards/components/FlashcardsDataGrid.utils.tsx
@@ -0,0 +1,78 @@
+import {
+  getGridStringOperators,
+  getGridNumericOperators,
+  getGridBooleanOperators,
+  getGridSingleSelectOperators,
+} from '@mui/x-data-grid';
+
+export const PAGE_SIZE_OPTIONS = [10, 20, 50];
+
+/**
+ * Mapping of DataGrid columns types with supported filter methods.
+ */
+export const mappedFilterOperators = {
+  string: getGridStringOperators().filter((operator) =>
+    ['contains'].includes(operator.value)
+  ),
+  number: getGridNumericOperators().filter((operator) =>
+    ['=', '>=', '<='].includes(operator.value)
+  ),
+  boolean: getGridBooleanOperators(),
+  singleSelect: getGridSingleSelectOperators().filter((operator) =>
+    ['is'].includes(operator.value)
+  ),
+};
+
+/**
+ * Function for formatting filterModel for API calls purposes.
+ * @param {object} updatedFilterModel - Updated filterModel returned from DataGrid after update.
+ * @param {object} columns - Displayed columns settings.
+ * @return {object} - Formatted filterModel for DataGrid.
+ */
+export function formatFilterModel(updatedFilterModel, columns) {
+  if (updatedFilterModel.items.length === 0) {
+    return {};
+  } else if (updatedFilterModel.items[0].value == null) {
+    return {};
+  }
+  const filterItem = updatedFilterModel.items[0];
+  const column = columns.find((column) => column.field === filterItem.field);
+  switch (column.type) {
+    case 'number':
+      return formatNumberFilter(filterItem);
+    default:
+      return { [filterItem.field]: filterItem.value };
+  }
+}
+
+/**
+ * Function for formatting number filter for API calls purposes.
+ * @param {object} filterItem - Filter definition received from DataGrid
+ * @return {object} - Formatted filterModel with number filter for DataGrid.
+ */
+function formatNumberFilter(filterItem) {
+  switch (filterItem.operator) {
+    case '>=': {
+      return { [`${filterItem.field}_min`]: filterItem.value };
+    }
+    case '<=':
+      return { [`${filterItem.field}_max`]: filterItem.value };
+    default:
+      return { [filterItem.field]: filterItem.value };
+  }
+}
+
+/**
+ * Function to prepare mapping of API ordering fields for DataTable columns other than column names.
+ * @param {object} columns - DataTable columns definitions.
+ * @return {object} - Mapping for sorting DataTable rows.
+ */
+export const getSortFieldMapping = (columns) => {
+  return columns.reduce((acc, column) => {
+    if (column.sortField) {
+      acc[column.field] = column.sortField;
+    }
+    return acc;
+  }, {});
+};
+
