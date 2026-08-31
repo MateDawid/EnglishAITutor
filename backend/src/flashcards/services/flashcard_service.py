6531d930a59c9860f2764a07d7062fd933b9b6d1
MateDawid <mateusiakdawid@gmail.com>
2026-08-31 10:49:58 +0200

    Service for update or create UserRating for flashcard

--- backend/src/flashcards/services/flashcard_service.py
+++ backend/src/flashcards/services/flashcard_service.py
@@ -1,12 +1,14 @@
 from typing import Any
+from uuid import UUID
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from flashcards.models.db_user_rating import Rating
 from utils.filtering.services.filtering_service import get_db_query_with_filtering
 from utils.sorting import get_db_query_with_ordering
-from flashcards.models import DbFlashcard
-from flashcards.schemas import FlashcardSchema
+from flashcards.models import DbFlashcard, DbUserRating
+from flashcards.schemas import FlashcardSchema, UserRatingSchema
 from utils.pagination import get_paginated_response, PaginationQuery, PaginatedResponse
 
 
@@ -35,3 +37,49 @@ async def get_flashcards_from_db(
         db=db, query=query, schema_type=FlashcardSchema, pagination_query=pagination_query
     )
     return response
+
+
+async def update_or_create_user_rating(
+    db: AsyncSession, user_id: UUID, flashcard_id: UUID, rating: Rating
+) -> UserRatingSchema:
+    """
+    Update or create a user rating for a flashcard.
+
+    Args:
+        db (AsyncSession): The database session to use for the query.
+        user_id (str): The ID of the user.
+        flashcard_id (str): The ID of the flashcard.
+        rating (int): The rating value.
+
+    Returns:
+        dict: A dictionary containing the status and message of the operation.
+    """
+
+    existing_rating = await _get_rating_from_db(db=db, flashcard_id=flashcard_id, user_id=user_id)
+    if existing_rating is None:
+        db.add(DbUserRating(user_id=user_id, flashcard_id=flashcard_id, rating=rating))
+        await db.commit()
+        return UserRatingSchema(rating_changed=True)
+    if existing_rating.rating == rating:
+        return UserRatingSchema(rating_changed=False)
+    existing_rating.rating = rating
+    await db.commit()
+    return UserRatingSchema(rating_changed=True)
+
+
+async def _get_rating_from_db(db: AsyncSession, flashcard_id: UUID, user_id: UUID) -> DbUserRating | None:
+    """
+    Get the user rating for a flashcard from the database.
+
+    Args:
+        db (AsyncSession): The database session to use for the query.
+        flashcard_id (UUID): The ID of the flashcard.
+        user_id (UUID): The ID of the user.
+
+    Returns:
+        DbUserRating | None: The user rating instance if found, otherwise None.
+    """
+    db_result = await db.execute(
+        select(DbUserRating).where(DbUserRating.user_id == user_id, DbUserRating.flashcard_id == flashcard_id)
+    )
+    return db_result.scalar_one_or_none()
