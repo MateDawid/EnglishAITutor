dc69ebbec30f6dc204bddd04895f5bf52f92f09b
MateDawid <mateusiakdawid@gmail.com>
2026-09-01 11:11:09 +0200

    Separate enums for database ratings and view filters ratings

--- backend/src/flashcards/models/db_user_rating.py
+++ backend/src/flashcards/models/db_user_rating.py
@@ -1,21 +1,11 @@
 import uuid
-from enum import IntEnum
 
 from sqlalchemy import Enum as SqlEnum, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
+from flashcards.enums import DatabaseRating
 from utils.database import Base
-
-
-class Rating(IntEnum):
-    """
-    Enum representing ratings for Flashcards given by users in the database.
-    """
-
-    EASY = 1
-    MEDIUM = 2
-    HARD = 3
 
 
 class DbUserRating(Base):
@@ -35,7 +25,7 @@ class DbUserRating(Base):
         ForeignKey("users.id"),
         primary_key=True,
     )
-    rating: Mapped[Rating] = mapped_column(
-        SqlEnum(Rating, name="rating_enum"),
+    rating: Mapped[DatabaseRating] = mapped_column(
+        SqlEnum(DatabaseRating, name="rating_enum"),
         nullable=False,
     )
