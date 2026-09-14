09731edd4ec29e5097ca384748da4610e16782c6
MateDawid <mateusiakdawid@gmail.com>
2026-09-14 13:10:47 +0200

    Update _get_user_rating_query docstring

--- backend/src/flashcards/services/utils.py
+++ backend/src/flashcards/services/utils.py
@@ -7,13 +7,13 @@ from flashcards.models import DbFlashcard, DbUserRating
 
 def _get_user_rating_query(user_id: UUID) -> ScalarSelect:
     """
-    Get the database subquery with User ratings for Flashcards.
+    Get the database subquery for User ratings for Flashcards.
+
     Args:
-        query (SelectType): The database query to extend.
         user_id (UUID): The ID of the User to get ratings for.
 
     Returns:
-        SelectType: The database query with User ratings for Flashcards.
+        ScalarSelect: The database query for User ratings for Flashcards.
     """
     return (
         select(DbUserRating.rating)
